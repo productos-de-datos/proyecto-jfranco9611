13e45d1b4392e9cc8a8484a9672c88f1f135fec3
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-07-01 18:06:02 -0500

    pr1

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,22 +1,25 @@
-"""
-Módulo de ingestión de datos.
--------------------------------------------------------------------------------
-
-"""
-
 
 def ingest_data():
-    """Ingeste los datos externos a la capa landing del data lake.
+    import wget
+    import os
+    os.chdir("data_lake/ladnding/")
+    for num in range(1995, 2022):
+        if num in range(2016, 2018):
+            wdir = 'https://github.com/jdvdelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(
+                num)
+            wget.download(wdir)
+        else:
+            wdir = 'https://githubd.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(
+                num)
+            wget.download(wdir)
+    os.chdir('../../')
 
-    Del repositorio jdvelasq/datalabs/precio_bolsa_nacional/xls/ descarge los
-    archivos de precios de bolsa nacional en formato xls a la capa landing. La
-    descarga debe realizarse usando únicamente funciones de Python.
-
-    """
-    raise NotImplementedError("Implementar esta función")
-
+def test_ruta_origen():
+    import os
+    assert set(os.listdir()) - set(['.git', '.github', '.gitignore', '.vscode', 'data_lake', 'grader.py', 'Makefile', 'README.md', 'src']) == set()
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    ingest_data()
