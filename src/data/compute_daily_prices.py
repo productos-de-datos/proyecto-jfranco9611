43c6ef5a04a2ef98865f15f699423263f01c03f2
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-07-01 18:04:59 -0500

    pr1

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -1,21 +1,17 @@
 def compute_daily_prices():
-    """Compute los precios promedios diarios.
+    import pandas as pd
+    df = pd.read_csv("data_lake/cleansed/Data_Prices.csv",
+                     index_col=None, header=0)
+    df = df[["date", "price"]]
+    df["date"] = pd.to_datetime(df["date"])
+    compute_daily_prices = df.groupby("date").mean({"price": "price"})
+    compute_daily_pric
+        "data_lake/business/Data_Prices.csv", index=None, header=True)
 
-    Usando el archivo data_lake/cleansed/precios-horarios.csv, compute el prcio
-    promedio diario (sobre las 24 horas del dia) para cada uno de los dias. Las
-    columnas del archivo data_lake/business/precios-diarios.csv son:
-
-    * fecha: fecha en formato YYYY-MM-DD
-
-    * precio: precio promedio diario de la electricidad en la bolsa nacional
-
-
-
-    """
-    raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    compute_daily_prices()
