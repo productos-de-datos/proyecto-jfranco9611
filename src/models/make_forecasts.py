840f105599b2db8d301e9420f4f980a0a55eca26
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-07-02 09:53:50 -0500

    pr1

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -1,21 +1,92 @@
 def make_forecasts():
-    """Construya los pronosticos con el modelo entrenado final.
+    from train_daily_model import train_daily_model, load_data, rev_data, make_train_test_split
 
-    Cree el archivo data_lake/business/forecasts/precios-diarios.csv. Este
-    archivo contiene tres columnas:
+## Se crean la primera función
+    def pkl_loading():
+        import pickle
+        with open("src/models/precios-diarios.pickle", "rb") as file:
+            Rea_Model = pickle.load(file)
+        return Rea_Model
 
-    * La fecha.
+## Se generan los estimadores
+    def score(x_train, y_train):
+        import numpy as np
 
-    * El precio promedio real de la electricidad.
+        estimators = np.arange(10, 200, 10)
+        scores = []
+        for n in estimators:
+            Rea_Model.set_params(n_estimators=n)
+            Rea_Model.fit(x_train, y_train)
+            scores.append(Rea_Model.score(x_test, y_test))
+        return scores
 
-    * El pronóstico del precio promedio real.
+## Se busca el mejor estimador
+    def best_score(scores):
+        import pandas as pd
+        
+        Bestim = pd.DataFrame(scores)
+        Bestim.reset_index(inplace=True)
+        Bestim = Bestim.rename(columns={0: 'scores'})
+        Bestim = Bestim[Bestim['scores'] == Bestim['scores'].max()]
+        Bestim['index'] = (Bestim['index']+1)*10
+        Bestim = Bestim['index'].iloc[0]
+        return Bestim
+
+## Se busca el mejor estimador
+    def btrain_estimator(Bestim):
+        from sklearn.ensemble import RandomForestRegressor
+
+        Rea_Model = RandomForestRegressor(n_estimators=Bestim, random_state=1017)
+        Rea_Model.fit(x_train, y_train)
+        return Rea_Model
+
+## Se testea la predicción del modelo
+    def prediction_test(Rea_Model):
+        yp_test = Rea_Model.predict(x_test)
+        return yp_test
+
+## se realiza el pronostico
+    def forecasts(yp_test, y_test, data):
+        import pandas as pd
+        import numpy as np
+
+        df_Rea_Model = pd.DataFrame(y_test).reset_index(drop=True)
+        df_Rea_Model['pronostico'] = yp_test
+        np.random.seed(0)
+
+        df_series = pd.Series(y_test)
+        df_series = df_series.to_frame()
+        df_series.reset_index(inplace=True)
+        df_series = df_series[['index']]
+
+        df_Rea_Model = pd.concat([df_Rea_Model, df_series], axis=1)
+        data = data[['date']]
+        data.reset_index(inplace=True)
+
+        df_Rea_Model = pd.merge(df_Rea_Model, data, on='index', how='left')
+        df_Rea_Model = df_Rea_Model[['date', 'price', 'pronostico']]
+        return df_Rea_Model
+
+## Se convierten los pronosticos
+    def save_forecasts(df_Rea_Model):
+        df_Rea_Model.to_csv('data_lake/business/forecasts/Data_Prices.csv', index=None)
 
 
-    """
-    raise NotImplementedError("Implementar esta función")
+## Ejecución del proceso
+    data = load_data()
+    x, y = rev_data(data)
+    x_train, x_test, y_train, y_test = make_train_test_split(x, y)
+    Rea_Model = pkl_loading()
+    scores = score(x_train, y_train)
+    Bestim = best_score(scores)
+    Rea_Model = btrain_estimator(Bestim)
+    yp_test = prediction_test(Rea_Model)
+    df_Rea_Model = forecasts(yp_test, y_test, data)
+    save_forecasts(df_Rea_Model)
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    make_forecasts()
--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -1,15 +1,68 @@
+def load_data():
+    import pandas as pd
+
+    in_path = 'data_lake/business/features/Data_Prices.csv'
+    data_pr = pd.read_csv(in_path, sep=",")
+    return data_pr
+
+
+def rev_data(data_pr):
+    import pandas as pd
+
+    df = data_pr.copy()
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+    df['year'], df['month'], df['day'] = df['date'].dt.year, df['date'].dt.month, df['date'].dt.day
+
+    y = df["price"]
+    x = df.copy()
+    x.pop("price")
+    x.pop("date")
+    return x, y
+
+
+def make_train_test_split(x, y):
+    from sklearn.model_selection import train_test_split
+
+    (x_train, x_test, y_train, y_test) = train_test_split(
+        x,
+        y,
+        test_size=0.25,
+        random_state=1017,
+    )
+    return x_train, x_test, y_train, y_test
+
+
+def model_training(x_train, x_test):
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.ensemble import RandomForestRegressor
+
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+    Rea_Model = RandomForestRegressor(n_jobs=-1)
+    return Rea_Model
+
+
+def save_model(Rea_Model):
+    import pickle
+
+    with open("src/models/Data_Price.pickle", "wb") as file:
+        pickle.dump(Rea_Model, file,  pickle.HIGHEST_PROTOCOL)
+
+
 def train_daily_model():
-    """Entrena el modelo de pronóstico de precios diarios.
+    data = load_data()
+    x, y = rev_data(data)
+    x_train, x_test, y_train, y_test = make_train_test_split(x, y)
+    Rea_Model = model_training(x_train, x_test)
+    save_model(Rea_Model)
 
-    Con las features entrene el modelo de proóstico de precios diarios y
-    salvelo en models/precios-diarios.pkl
-
-
-    """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    train_daily_model()
