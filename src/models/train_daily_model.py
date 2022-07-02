0fff1e5e41d7f1919583d3124c921d3839deaade
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-07-02 11:33:02 -0500

    pr2

--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -1,68 +1,26 @@
-def load_data():
-    import pandas as pd
-
-    in_path = 'data_lake/business/features/Data_Prices.csv'
-    data_pr = pd.read_csv(in_path, sep=",")
-    return data_pr
-
-
-def rev_data(data_pr):
-    import pandas as pd
-
-    df = data_pr.copy()
-    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
-    df['year'], df['month'], df['day'] = df['date'].dt.year, df['date'].dt.month, df['date'].dt.day
-
-    y = df["price"]
-    x = df.copy()
-    x.pop("price")
-    x.pop("date")
-    return x, y
-
-
-def make_train_test_split(x, y):
-    from sklearn.model_selection import train_test_split
-
-    (x_train, x_test, y_train, y_test) = train_test_split(
-        x,
-        y,
-        test_size=0.25,
-        random_state=1017,
-    )
-    return x_train, x_test, y_train, y_test
-
-
-def model_training(x_train, x_test):
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.ensemble import RandomForestRegressor
-
-    scaler = StandardScaler()
-    scaler.fit(x_train)
-    x_train = scaler.transform(x_train)
-    x_test = scaler.transform(x_test)
-    Rea_Model = RandomForestRegressor(n_jobs=-1)
-    return Rea_Model
-
-
-def save_model(Rea_Model):
-    import pickle
-
-    with open("src/models/Data_Price.pickle", "wb") as file:
-        pickle.dump(Rea_Model, file,  pickle.HIGHEST_PROTOCOL)
-
-
 def train_daily_model():
-    data = load_data()
-    x, y = rev_data(data)
-    x_train, x_test, y_train, y_test = make_train_test_split(x, y)
-    Rea_Model = model_training(x_train, x_test)
-    save_model(Rea_Model)
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from sklearn import linear_model
+    from sklearn.metrics import mean_squared_error
+    import numpy as np
+    import pickle
+    df = pd.read_csv('data_lake/business/features/precios_diarios.csv')
+    df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
+    df['weekday'] = pd.to_numeric(df['weekday'])
 
-    #raise NotImplementedError("Implementar esta función")
+    X = np.array(df['weekday']).reshape(-1, 1)
+    y = np.array(df['precio']).reshape(-1, 1)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=1017
+    )
+    regr = linear_model.LinearRegression()
+    model = regr.fit(X_train, y_train)
+    pickle.dump(model, open("src/models/precios-diarios.pkl", "wb"))
+
 
 
 if __name__ == "__main__":
     import doctest
-
-    doctest.testmod()
     train_daily_model()
+    doctest.testmod()
