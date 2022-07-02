c2e3187977312d17cde0ae2dc29450b8cfa1fadc
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-07-02 11:43:01 -0500

    pr1

--- src/visualization/make_monthly_prices_plot.py
+++ src/visualization/make_monthly_prices_plot.py
@@ -1,12 +1,12 @@
-def make_daily_prices_plot():
+def make_monthly_prices_plot():
     import pandas as pd
-    df_m = pd.read_csv('data_lake/business/precios-diarios.csv', index_col=0)
+    df_m = pd.read_csv('data_lake/business/precios-mensuales.csv', index_col=0)
     ax =df_m['precio'].plot(figsize=(15,5), kind='line', grid=True, x_compat=False)
     fig = ax.get_figure()
-    fig.savefig('data_lake/business/reports/figures/daily_prices.png')
+    fig.savefig('data_lake/business/reports/figures/monthly_prices.png')
     print("Saved ---")
 
 if __name__ == "__main__":
     import doctest
-    make_daily_prices_plot()
+    make_monthly_prices_plot()
     doctest.testmod()
