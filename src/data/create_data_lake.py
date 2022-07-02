897aee382d459a04c07d24a63d28a3e9bafe8d42
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-07-01 19:09:04 -0500

    pr3

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -3,10 +3,11 @@ def create_data_lake():
     os.mkdir('./data_lake/')
     parent_dir = 'data_lake/'
     carpetas = ['landing', 'raw', 'cleansed', 'business']
-    [os.mkdir(os.path.join(parent_dir, )) for c in carpetas]
+    [os.mkdir(os.path.join(parent_dir, c)) for c in carpetas]
     parent_dir = 'data_lake/business/'
-    carpetas = ['reports', 'features', 'forecasts']
-
+    [os.mkdir(os.path.join(parent_dir, c)) for c in carpetas]
+    parent_dir = 'data_lake/business/reports/'
+    directory = 'figures'
     os.mkdir(os.path.join(parent_dir, directory))
 
 
