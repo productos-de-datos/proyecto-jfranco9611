5fcbadbba5f049ab9e9337beacb369ece9892908
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-07-03 13:50:51 -0500

    pr3

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,12 +1,22 @@
+"""Crea un grafico de lines que representa los precios promedios diarios"""
+import doctest
+import pandas as pd
 def make_daily_prices_plot():
-    import pandas as pd
+    """Crea un grafico de lines que representa los precios promedios diarios.
+
+    Usando el archivo data_lake/business/precios-diarios.csv, crea un grafico de
+    lines que representa los precios promedios diarios.
+
+    El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
+
+    """
     df_m = pd.read_csv('data_lake/business/precios-diarios.csv', index_col=0)
-    ax =df_m['precio'].plot(figsize=(15,5), kind='line', grid=True, x_compat=False)
-    fig = ax.get_figure()
+    ax_plot =df_m['precio'].plot(figsize=(15,5), kind='line', grid=True, x_compat=False)
+    fig = ax_plot.get_figure()
     fig.savefig('data_lake/business/reports/figures/daily_prices.png')
     print("Saved ---")
 
 if __name__ == "__main__":
-    import doctest
+
     make_daily_prices_plot()
     doctest.testmod()
