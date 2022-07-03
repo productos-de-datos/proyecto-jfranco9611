cf26e2bb9085d9ddcb4e92b7e3c9d0bbc0a8cd3c
jfranco9611 <101428721+jfranco9611@users.noreply.github.com>
2022-07-03 13:39:53 -0500

    pr3

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,4 +1,17 @@
 def make_features():
+    """Prepara datos para pronóstico.
+
+    Cree el archivo data_lake/business/features/precios-diarios.csv. Este
+    archivo contiene la información para pronosticar los precios diarios de la
+    electricidad con base en los precios de los días pasados. Las columnas
+    correspoden a las variables explicativas del modelo, y debe incluir,
+    adicionalmente, la fecha del precio que se desea pronosticar y el precio
+    que se desea pronosticar (variable dependiente).
+
+    En la carpeta notebooks/ cree los notebooks de jupyter necesarios para
+    analizar y determinar las variables explicativas del modelo.
+
+    """
     import pandas as pd
     df = pd.read_csv('data_lake/business/precios-diarios.csv')
     df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
@@ -7,8 +20,8 @@ def make_features():
     print ("Saved features--->")
     return True
 
+
 if __name__ == "__main__":
     import doctest
-
-    doctest.testmod()
     make_features()
+    doctest.testmod()
